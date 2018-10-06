d880ddce29d52c3400fcc553692ce7269de5b4cf
Hemory Phifer <hemory@Hemorys-MacBook-Pro.local>
2018-10-06 17:55:21 -0400

    Initial Commit

---
+++ MovieCapstone/Menu.cs
@@ -0,0 +1,128 @@
+﻿using System;
+namespace MovieCapstone
+{
+    public class Menu
+    {
+        public void ShowMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------------------------------------------");
+            Console.WriteLine("1='Show Movie List', 2='Find a movie', 3='Find actor/director', 4='Quit'");
+            Console.WriteLine("----------------------------------------------------------------------");
+            Console.WriteLine();
+        }
+
+
+
+
+
+        public void MakeChoice(int choice)
+        {
+            var menu = new Menu();
+            var movie = new Movie();
+            var userInput = string.Empty;
+            switch (choice)
+            {
+                case 1:
+                    movie.ShowMovieList();
+                    menu.MakeAnotherSelection();
+                    break;
+                case 2:
+                    menu.FindMovieByTitle(userInput);
+                    menu.MakeAnotherSelection();
+                    break;
+                default:
+                    Console.WriteLine("Unknown command");
+                    break;
+            }
+        }
+
+        public void MakeAnotherSelection()
+        {
+            Console.WriteLine("Would you like to conintue Y/N? ");
+            var userInput = Console.ReadLine().ToLower();
+
+
+            if (userInput == "y")
+            {
+                //var movieMenu = new Menu();
+                ShowMenu();
+                var choice = Convert.ToInt32(Console.ReadLine());
+                MakeChoice(choice);
+                //MakeAnotherSelection();
+            }
+            else
+            {
+                Console.WriteLine("Thank you for choosing Move App!");
+            }
+
+        }
+
+        public void FindMovieByTitle(string title)
+        {
+            var menu = new Menu();
+            Console.WriteLine("Enter the title of the movie you wish to find?");
+            title = Console.ReadLine();
+            var movies = new Movie();
+            var movieList = movies.LoadMovies();
+           var result = movieList.Find(x => x.Title.ToUpper() == title.ToUpper());
+            if (result == null)
+            {
+                Console.WriteLine($"Cant find the movie called {title}");
+            }
+            else
+            {
+               
+                Console.WriteLine($"ID: {result.ID} Title: {result.Title} Actor: {result.Actor} Genre: {result.Genre} Director: {result.Director}");
+
+            }
+
+
+
+        }
+  
+
+         /*   public void AddMovie()
+            {
+                string title;
+                AskforTitle(out title, "Enter the title of the movie: ");
+
+
+                ProductionYear productionYear;
+                AskFor(out productionYear, "When was the movie produced?");
+
+                Movie movie = new Movie(title, productionYear);
+                _movieService.Add(movie);
+
+                string name;
+                AskFor(out name, "Enter the name of the director.");
+                CastOrCrew director = _castOrCrewService.FindBy(name);
+                if (director == null)
+                {
+                    director = NewCastOrCrew(name);
+                }
+
+                _castOrCrewService.AddPeople(director);
+                movie.Director = director;
+
+                bool keepAskingForNewActor = false;
+                do
+                {
+                    AskFor(out name, "Enter the name of an actor: ");
+                    CastOrCrew actor = _castOrCrewService.FindBy(name);
+                    if (actor == null)
+                    {
+                        actor = NewCastOrCrew(name);
+                    }
+                    _castOrCrewService.AddPeople(actor);
+                    movie.AddActor(actor);
+                    Console.Write("Do you want to add a new actor to the movie? (y/n)");
+                    char c = Console.ReadKey().KeyChar;
+                    keepAskingForNewActor = (char.ToLower(c) == 'y');
+                    Console.WriteLine();
+                } while (keepAskingForNewActor); */
+
+            } 
+        }
+    
+
---
+++ MovieCapstone/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieCapstone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PRINTS OUT THE MENU
+
+            Console.WriteLine("Hello, welcome to the Movie App. Please select an option from the menu below.");
+
+            var movieMenu = new Menu();
+            movieMenu.ShowMenu();
+            var choice = Convert.ToInt32(Console.ReadLine());
+            movieMenu.MakeChoice(choice);
+
+
+
+
+
+        }
+
+      
+    }
+
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Actor { get; set; }
+        public string Genre { get; set; }
+        public string Director { get; set; }
+
+        public List<Movie> LoadMovies()
+        {
+            //SHOWS A LIST OF MOVIES
+
+            List<Movie> MovieList = new List<Movie>
+            {
+                new Movie { ID = 1, Title = "THE GREEN MILE", Actor = "GUAM HERNANDEZ", Genre = "FUNNY", Director = "Juan" },
+                new Movie { ID = 2, Title = "MEN IN BLACK", Actor = "WILL SMITH", Genre = "SAD", Director = "Juan" },
+                new Movie { ID = 3, Title = "GHOST", Actor = "PATRICK SWAYZE", Genre = "COMEDY", Director = "Juan" },
+                new Movie { ID = 4, Title = "FAST AND FURIOUS", Actor = "VIN DIESEL", Genre = "DARK", Director = "Juan" },
+                new Movie { ID = 5, Title = "THE NOTEBOOK", Actor = "RYAN GOSLING", Genre = "ACTION", Director = "Juan" },
+                new Movie { ID = 6, Title = "JAWS", Actor = "GABRIEL UNION", Genre = "ROMANCE", Director = "Juan" },
+                new Movie { ID = 7, Title = "TITANIC", Actor = "LEO DECRAPIO", Genre = "CRYBABY", Director = "Juan" }
+            };
+
+            return MovieList;
+                
+        }
+
+        public void ShowMovieList()
+        {
+            var movies = new Movie();
+           var movieList = movies.LoadMovies();
+
+            foreach (var movie in movieList)
+            {
+                Console.WriteLine($"ID: {movie.ID} Title: {movie.Title} Actor: {movie.Actor} Genre: {movie.Genre} Director: {movie.Director}");
+            }
+        }
+
+
+
+
+
+
+    }
+}
+
+
+
